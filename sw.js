9699fb196f91c01723290604ad7df52c92be36b1
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2021-06-10 10:38:22 +0300

    set fetch events sync

--- sw.js
+++ sw.js
@@ -10,9 +10,14 @@ const assetsUrls = [
   "Bells2.mp3",
 ];
 
-self.addEventListener("install", async (event) => {
-  const cache = await caches.open(staticCacheName);
-  await cache.addAll(assetsUrls);
+// self.addEventListener("install", async (event) => {
+//   const cache = await caches.open(staticCacheName);
+//   await cache.addAll(assetsUrls);
+// });
+
+self.addEventListener("install", (event) => {
+  const cache = caches.open(staticCacheName);
+  cache.addAll(assetsUrls);
 });
 
 self.addEventListener("fetch", (event) => {
@@ -21,7 +26,9 @@ self.addEventListener("fetch", (event) => {
 
 // cache strategy
 async function cacheFirst(request) {
-  const cached = await caches.match(request);
+  // const cached = await caches.match(request);
+  const cached = caches.match(request);
+  
   // return cached ?? (await fetch(request));
   return cached;
 }
