355d511a692180dfbd2f1cb1474df47c28d6505f
StanislavKaisin <41913995+StanislavKaisin@users.noreply.github.com>
2021-06-14 13:09:23 +0300

    proxy error response

--- sw.js
+++ sw.js
@@ -37,3 +37,25 @@ self.addEventListener("activate", async (event) => {
       .map((name) => caches.delete(name))
   );
 });
+
+/*
+General
+Request URL: https://stanislavkaisin.github.io/personaltimer/Bells2.mp3
+Request Method: GET
+Status Code: 407 authenticationrequired (from disk cache)
+Referrer Policy: strict-origin-when-cross-origin
+
+Response Headers
+Content-Length: 4010
+Proxy-Authenticate: NTLM
+Proxy-Authenticate: Basic realm="KOVALSKY PROXY"
+Proxy-Connection: Keep-Alive
+
+Request headers
+Accept-Encoding: identity;q=1, *;q=0
+Range: bytes=0-
+Referer: https://stanislavkaisin.github.io/personaltimer/
+sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"
+sec-ch-ua-mobile: ?1
+User-Agent: Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36
+*/
